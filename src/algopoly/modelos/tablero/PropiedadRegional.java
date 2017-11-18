47f9b32b8fb28b0b8fbf41af8e471acd17a4488f
ndvazquez <vazquez.nicolas.daniel@gmail.com>
2017-11-18 00:43:03 -0300

    Refactorización de PropiedadRegional, ahora hereda de PropiedadSimple

--- src/algopoly/modelos/tablero/PropiedadRegional.java
+++ src/algopoly/modelos/tablero/PropiedadRegional.java
@@ -1,33 +1,12 @@
 package algopoly.modelos.tablero;
 
-import algopoly.modelos.jugador.Jugador;
-
-public class PropiedadRegional implements Casillero, Propiedad {
-
-    private Jugador propietario;
-
-    private Integer precio;
+public class PropiedadRegional extends PropiedadSimple implements Casillero, Propiedad{
 
     private Region region;
 
     public PropiedadRegional(Integer precio, Region region){
-        this.precio = precio;
+        super(precio);
         this.region = region;
     }
 
-    @Override
-    public void recibirJugador(Jugador jugador) {
-        jugador.comprarPropiedad(this);
-        this.propietario = jugador;
-    }
-
-    @Override
-    public Jugador getPropietario() {
-        return this.propietario;
-    }
-
-    @Override
-    public Integer getPrecio(){
-        return this.precio;
-    }
 }

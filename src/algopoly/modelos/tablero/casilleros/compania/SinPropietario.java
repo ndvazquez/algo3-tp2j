ae04f2706088d3baad89b44627d3ec43c8cdc05f
Camila Serra <camilaserra5@gmail.com>
2017-11-30 23:05:38 -0300

    se agrega la opción de comprar o no barrios y companias

--- src/algopoly/modelos/tablero/casilleros/compania/SinPropietario.java
+++ src/algopoly/modelos/tablero/casilleros/compania/SinPropietario.java
@@ -6,8 +6,12 @@ public class SinPropietario implements Estado {
 
 	@Override
 	public void recibirJugador(Jugador jugador, Compania compania) {
+
+	}
+	
+	@Override
+	public void comprarCompania(Jugador jugador, Compania compania) {
 		jugador.comprarCompania(compania);
 		compania.setPropietario(jugador);
-
 	}
 }
